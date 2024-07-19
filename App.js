0b7f8343bc75025ba7cc7b708bfc2ee8783c5d62
venky67uv <venkatesh.ukoti@kunchams.com>
2024-07-19 17:28:39 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,15 @@
+
+
+const parent =React.createElement("div",{id:"parent"},[
+    React.createElement("div", {id:"child"},[
+        React.createElement("h1",{},"Iam an h1 tag"),
+        React.createElement("h2",{},"Iam an h2 tag"),
+       ]),
+    React.createElement("div", {id:"child2"},[
+    React.createElement("h1",{},"Iam an h1 tag"),
+    React.createElement("h2",{},"Iam an h2 tag"),
+    ]),
+  ]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
